0bb21c7419f2751a93daae6edc34d93280fb928f
ristaa <rista90@gmail.com>
2019-01-16 12:01:28 +0100

    node send mail

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
 const PORT = process.env.PORT || 5000;
 
 const server = 'ds147723.mlab.com:47723';
@@ -27,6 +28,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(express.multipart());
 
 app.use((res, req, next) => {
   res.header('Access-Control-Allow-Origin', '*');
